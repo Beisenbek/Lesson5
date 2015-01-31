02390564fae747629a9c6426a097e4652d86cecf
beysenbek <beysenbek@gmail.com>
2015-01-31 23:15:58 +0400

    Lesson5

---
+++ RegExp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace RegExp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = "^t(es){2,}t$";
+            Regex r = new Regex(pattern);
+            string text =  Console.ReadLine();
+            if (r.IsMatch(text))
+            {
+                Console.WriteLine("ok");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
