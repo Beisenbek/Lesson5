a1d144457c60bd31340b38175bcbcb6226e2e2a3
beysenbek <beysenbek@gmail.com>
2015-02-01 00:04:24 +0400

    fix

---
+++ RegExp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace RegExp2
+{
+    public class Program
+    {
+        public static string pattern2 = "^[0-9]{12}$";
+        public static string pattern = "^[0-9]{12}$";
+            
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Console.WriteLine(MyMatch(pattern2,text));
+        }
+
+        public static string MyMatch(string pattern, string input)
+        {
+            Regex r = new Regex(pattern);
+           
+            if (r.IsMatch(input))
+            {
+                return "ok";
+            }
+            else
+            {
+                return "no";
+            }
+        }
+    }
+}
---
+++ RegExp3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace RegExp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = "[a-z]+";
+            string text = "test111test";
+
+            Regex r = new Regex(pattern);
+            Match m = r.Match(text);
+                
+            while(m.Success)
+            {
+                Console.WriteLine(m.Value);
+                m = m.NextMatch();
+            }
+
+
+                foreach (Match m2 in r.Matches(text))
+                {
+                    Console.WriteLine(m2.Value);
+                }
+            
+        }
+    }
+}
